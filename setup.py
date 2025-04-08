198f353644fec7faae62dc1de22ac0762ce8dab8
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-08 11:47:24 +0500

    ` "Nomi va versiyasini o'zgartirish: 'autogit' -> 'auto-git-tool', versiya 0.1 -> 1.2.0"`

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autogit",
-    version="0.1",
+    name="auto-git-tool",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
         'jst-aicommit',
