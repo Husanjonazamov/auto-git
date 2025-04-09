4a31bf698bd2ca33e2a7cb9ac7e78394ff17b8e8
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-09 18:24:56 +0500

    main branchni yangilash va versiya 1.2.3 ga oshirish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto-git-tool",
-    version="1.2.3",
+    version="1.2.4",
     packages=find_packages(),
     install_requires=[
         'jst-aicommit',
